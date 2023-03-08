45c036cfbd7460b6606f9ed030f11ad5d82e13cf
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2023-03-08 23:42:44 +0600

    saving list of User & Unit Testing it

--- spring-tester/src/test/java/com/adibahan/springtester/repository/EmployeeRepositoryTest.java
+++ spring-tester/src/test/java/com/adibahan/springtester/repository/EmployeeRepositoryTest.java
@@ -3,10 +3,13 @@ package com.adibahan.springtester.repository;
 import com.adibahan.springtester.model.Employee;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 @DataJpaTest
 class EmployeeRepositoryTest {
@@ -32,4 +35,37 @@ class EmployeeRepositoryTest {
         assertThat(savedEmployee.getId()).isGreaterThan(0);
 
     }
+
+    @DisplayName("Saving All Users ")
+    @Test
+    void savingListOfUsers() {
+
+        //given (precondition or setup)
+        Employee employee = Employee.builder()
+                .firstName("adib")
+                .lastName("chowdhury")
+                .email("email@ecom.com")
+                .build();
+        Employee employee2 = Employee.builder()
+                .firstName("adib")
+                .lastName("chowdhury")
+                .email("email2@ecom.com")
+                .build();
+        Employee employee3 = Employee.builder()
+                .firstName("adib")
+                .lastName("chowdhury")
+                .email("email3@ecom.com")
+                .build();
+
+        List<Employee> employeeList = List.of(employee, employee2, employee3);
+
+        //when (action or behavior that we are testing
+        List<Employee> savedEmployeeList = employeeRepository.saveAll(employeeList);
+
+        //verify the output
+        assertThat(savedEmployeeList).isNotNull();
+        assertThat(savedEmployeeList.size()).isEqualTo(3);
+    }
+
+
 }
